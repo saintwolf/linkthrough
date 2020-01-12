a86e32de889c233c41fe5184a56931e8d33e9e26
John Good <judiconis@gmail.com>
2020-01-12 13:00:29 +0000

    fixes

--- linkthrough/linkthroughapp/admin.py
+++ linkthrough/linkthroughapp/admin.py
@@ -4,5 +4,8 @@ from linkthroughapp.models import Link, LinkVisit
 class LinkAdmin(admin.ModelAdmin):
 	list_display = ('get_short_url', 'url')
 
+class LinkVisitAdmin(admin.ModelAdmin):
+	list_display = ('hashed_ip_address', 'link')
+
 admin.site.register(Link, LinkAdmin)
 admin.site.register(LinkVisit)
