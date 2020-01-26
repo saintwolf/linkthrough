9757bdb48dc0f3bd45d036e93133cd82b1b8e469
John Good <judiconis@gmail.com>
2020-01-26 14:43:13 +0000

    Added name functionality

--- linkthrough/linkthroughapp/admin.py
+++ linkthrough/linkthroughapp/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from linkthroughapp.models import Link, LinkVisit
 
 class LinkAdmin(admin.ModelAdmin):
-	list_display = ('get_short_url', 'url')
+	list_display = ('name', 'get_short_url', 'url')
 
 class LinkVisitAdmin(admin.ModelAdmin):
 	list_display = ('hashed_ip_address', 'link')
--- linkthrough/linkthroughapp/models.py
+++ linkthrough/linkthroughapp/models.py
@@ -2,28 +2,32 @@ from django.db import models
 import short_url
 
 class Link(models.Model):
+    name = models.CharField(
+        max_length = 32,
+        blank = True
+    )
     url = models.CharField(
-    	max_length = 255
+        max_length = 255
     )
 
     def get_short_url(self):
-    	return short_url.encode_url(self.pk)
+        return short_url.encode_url(self.pk)
 
 
     def __str__(self):
-        return "http://35.189.97.205/link/{}".format(short_url.encode_url(self.pk))
+        return "{} - http://35.189.97.205/link/{}".format(self.name, short_url.encode_url(self.pk))
 
 class LinkVisit(models.Model):
     hashed_ip_address = models.CharField(
-    	max_length = 56
+        max_length = 56
     )
     time_visited = models.DateTimeField(
-    	auto_now_add = True
+        auto_now_add = True
     )
     link = models.ForeignKey(
-    	'Link',
-    	on_delete = models.CASCADE,
-		related_name = 'visits'
+        'Link',
+        on_delete = models.CASCADE,
+        related_name = 'visits'
     )
 
     def __str__(self):
