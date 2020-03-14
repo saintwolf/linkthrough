9904582d15f14eead7d31fb31a580db4055734bd
John Good <judiconis@gmail.com>
2020-03-14 14:50:42 +0000

    Added link stats view

--- linkthrough/linkthroughapp/admin.py
+++ linkthrough/linkthroughapp/admin.py
@@ -5,7 +5,12 @@ class LinkAdmin(admin.ModelAdmin):
 	list_display = ('get_short_url', 'name', 'url')
 
 class LinkVisitAdmin(admin.ModelAdmin):
-	list_display = ('hashed_ip_address', 'link')
+	list_display = ('hashed_ip_address', 'time_visited')
+	def link_link(self, obj):
+		return mark_safe('<a href="{}">{}</a>'.format(
+		reverse("admin:link_change", args=(obj.link.pk,)),
+			obj.link.url
+        ))
 
 admin.site.register(Link, LinkAdmin)
 admin.site.register(LinkVisit)
--- linkthrough/linkthroughapp/views.py
+++ linkthrough/linkthroughapp/views.py
@@ -1,56 +1,63 @@
 from django.http import Http404, HttpResponse
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect, render
 import hashlib
 import short_url
 
 from .models import Link, LinkVisit
 
 def index(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+	x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
 
-    if x_forwarded_for:
-        ip_address = x_forwarded_for.split(',')[0]
-    else:
-        ip_address = request.META.get('REMOTE_ADDR')
+	if x_forwarded_for:
+		ip_address = x_forwarded_for.split(',')[0]
+	else:
+		ip_address = request.META.get('REMOTE_ADDR')
 
-    hash = hashlib.sha224(ip_address.encode('utf-8')).hexdigest()
+	hash = hashlib.sha224(ip_address.encode('utf-8')).hexdigest()
 
-    l = LinkVisit(hashed_ip_address=hash)
-    l.save()
+	l = LinkVisit(hashed_ip_address=hash)
+	l.save()
 
-    return HttpResponse("IP is: {}<br />Hash is: {}".format(ip_address, hash))
+	return HttpResponse("IP is: {}<br />Hash is: {}".format(ip_address, hash))
 
 def redirect_view(request, tiny):
-    link = get_link_from_short(tiny)
+	link = get_link_from_short(tiny)
 
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+	x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
 
-    if x_forwarded_for:
-        ip_address = x_forwarded_for.split(',')[0]
-    else:
-        ip_address = request.META.get('REMOTE_ADDR')
+	if x_forwarded_for:
+		ip_address = x_forwarded_for.split(',')[0]
+	else:
+		ip_address = request.META.get('REMOTE_ADDR')
 
-    hash = hashlib.sha224(ip_address.encode('utf-8')).hexdigest()
+	hash = hashlib.sha224(ip_address.encode('utf-8')).hexdigest()
 
-    l = LinkVisit(hashed_ip_address=hash, link=link)
-    l.save()
+	l = LinkVisit(hashed_ip_address=hash, link=link)
+	l.save()
 
-    return redirect(link.url)
+	return redirect(link.url)
 
 def link_stats_view(request, tiny):
 
-    link = get_link_from_short(tiny)
+	link = get_link_from_short(tiny)
 
-    visit_count = link.visits.count()
-    unique_visit_count = link.visits.values('hashed_ip_address').distinct().order_by().count()
+	visit_count = link.visits.count()
+	unique_visit_count = link.visits.values('hashed_ip_address').distinct().order_by().count()
+	link_visits = link.visits.order_by('-time_visited')
+	context = {
+		'link_visits': link_visits,
+		'visit_count': visit_count,
+		'unique_visit_count': unique_visit_count
+	}
 
-    return HttpResponse("Total Redirects: {}<br />Unique Redirects: {}".format(visit_count, unique_visit_count))
+	#return HttpResponse("Total Redirects: {}<br />Unique Redirects: {}".format(visit_count, unique_visit_count))
+	return render(request, 'linkthroughapp/link_stats_view.html', context)
 
 def get_link_from_short(tiny):
-    try:
-        id = short_url.decode_url(tiny)
-    except ValueError:
-        raise Http404('Bad encoded ID.')
+	try:
+		id = short_url.decode_url(tiny)
+	except ValueError:
+		raise Http404('Bad encoded ID.')
 
-    link = get_object_or_404(Link, pk=id)
-    return link
+	link = get_object_or_404(Link, pk=id)
+	return link
